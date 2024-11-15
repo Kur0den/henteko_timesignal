5027d6198d2d460be26737ed01ee3e26ee786997
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2024-11-15 18:45:51 +0900

    なんかいい感じにcommit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 class TimeSignal {
     static void Main() {
+        string path = "time.json";
 
+        try {
+            string content = File.ReadAllText(path);
+            Console.WriteLine(content);
+        } catch (FileNotFoundException) {
+            int[] newContent = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+        }
     }
 }
